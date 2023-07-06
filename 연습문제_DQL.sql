a2aec9396a0477256a2e37e79593d9e6bc4ae406
whtjqls0504 <whtjqls0504@naver.com>
2023-07-06 17:50:36 +0900

    230706

--- 연습문제_DQL.sql
+++ 연습문제_DQL.sql
@@ -30,19 +30,23 @@ SELECT *
 
 -- 6. EMPLOYEES 테이블에서 HIRE_DATE가 2005년도인 사원을 조회하시오. 
 -- DATE 타입.
-SELECT HIRE_DATE
-  FROM EMPLOYEES
- WHERE HIRE_DATE >= 2005;
- 
+
  
 -- 7. EMPLOYEES 테이블에서 MANAGER_ID가 없는 사원을 조회하시오.
 
 
 -- 8. EMPLOYEES 테이블에서 COMMISSION_PCT를 받는 사원들의 실제 커미션을 조회하시오. 커미션은 COMMISSION_PCT * SALARY로 계산하시오.
-
-
+SELECT AVG(COMMISSION_PCT * SALARY)
+ FROM EMPLOYEES;
+ 
 -- 9. EMPLOYEES 테이블에서 FIRST_NAME이 'J'로 시작하는 사원을 조회하시오.
-
+SELECT FIRST_NAME
+     , INSTR(FIRST_NAME, 'J')
+  FROM EMPLOYEES;
+  
+SELECT FIRST_NAME
+  FROM EMPLOYEES
+ WHERE FIRST_NAME LIKE CONCAT(CONCAT('%','J') , '%');
 
 -- 10. EMPLOYEES 테이블에서 JOB_ID가 'MAN'으로 끝나는 사원들의 MANAGER_ID를 중복을 제거하여 조회하시오.
 
